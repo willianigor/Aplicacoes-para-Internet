90943ca17bc977d65637adc13e9e6a9747e7b4e0
Igor Willian <igorwillian2023@gmail.com>
2025-05-22 17:45:14 -0300

    Introducao a JavaScript

---
+++ provaA2/q2_a2.js
@@ -0,0 +1,20 @@
+let l = 5
+let c = 10
+let a = c * l
+console.log(`A largura equivale a ${l}`)
+console.log(`O comprimento equivale a ${c}`)
+console.log(`A area equivale a ${a}`)
+
+let text = "Essa foi uma prova facil"
+let primeira_letra = text.charAt(0)
+let ultima_letra = text.charAt(text.length - 1)
+console.log(`A primeira letra do texto ${text} eh a letra: ${primeira_letra}`)
+console.log(`A ultima letra do texto ${text} eh a letra: ${ultima_letra}`)
+
+let peso = 85
+let altura = 1.7
+let imc = peso / Math.pow(altura,2)
+console.log(`A altura equivale a ${altura}`)
+console.log(`O peso equivale a ${peso}`)
+console.log(`O IMC equivale a ${imc}`)
+
